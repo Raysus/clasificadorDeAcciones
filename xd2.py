43c5b9d005fa7a22a4cb9616e7744c5d08e5f5c7
Raysus <dissapear.raysus@gmail.com>
2018-01-09 16:24:18 -0300

    ordena por tiempo y fecha

--- xd2.py
+++ xd2.py
@@ -9,11 +9,11 @@ data = data[data["perfil"] != "profesor"]
 
 #pasar datos del csv al dataframe
 df = pandas.DataFrame(data)
-df = df.sort_values(by=['id_usuario'])
+df = df.sort_values(by=['id_usuario', 'date', 'time'])
 
 #agrupar por id_usuario
 df_by_idusuario = df.groupby('id_usuario')
-
+   
 # Check type of GroupBy object
 # print(type(df_by_idusuario))
 
@@ -23,4 +23,4 @@ df_by_idusuario = df.groupby('id_usuario')
 #    print(df_usuario.groups)
     #print(df_usuario)
 
-print(list(df_by_idusuario)[2])
+print(list(df_by_idusuario)[3])
